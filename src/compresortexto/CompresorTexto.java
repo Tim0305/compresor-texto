52f741c165f4b42e74c057d7e0133976690827e1
timoteo <tabtim1015@gmail.com>
2024-10-02 09:27:04 -0600

    Feat: GUI v2. Colores en los botones

--- src/compresortexto/CompresorTexto.java
+++ src/compresortexto/CompresorTexto.java
@@ -1,76 +1,10 @@
 package compresortexto;
 
 import gui.CompresorTextoGUI;
-import lz77.compresor.Compresor;
-import lz77.PaqueteService;
-import java.util.List;
-import java.util.Scanner;
-import lz77.Lz77Compresor;
 
 public class CompresorTexto {
 
     public static void main(String[] args) {
-
-//        /*
-//        TODO:
-//        1. Comprimir
-//            1.1 Obtener el texto
-//            1.2 Obtener la salida generada equivalente al texto comprimido
-//            1.3 Imprimir la salida
-//
-//        2. Descomprimir
-//            2.1 Obtener el código
-//            2.2 Obtener la salida generada equivalente al texto
-//            2.3 Imprimir la salida
-//         */
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("*****************************************");
-//        System.out.println("           Compresor de texto");
-//        System.out.println("*****************************************");
-//
-//        System.out.println();
-//        System.out.println("(q) para salir");
-//        System.out.println();
-//
-//        // Variables
-//        String op;
-//        String input;
-//        while (true) {
-//
-//            System.out.println("[1] Comprimir");
-//            System.out.println("[2] Descomprimir");
-//            System.out.println();
-//            System.out.print("-> ");
-//            op = scanner.nextLine();
-//            System.out.println();
-//
-//            switch (op) {
-//                case "q":
-//                    return;
-//
-//                case "1":
-//                    // Comprimir
-//                    System.out.print("Ingrese el texto que quiere comprimir: ");
-//                    input = scanner.nextLine();
-//                    System.out.print("Resultado: ");
-//                    System.out.println(Lz77Compresor.comprimir(input));
-//                    break;
-//
-//                case "2":
-//                    // Descomprimir
-//                    System.out.print("Ingrese el codigo que quiere descomprimir: ");
-//                    input = scanner.nextLine();
-//                    System.out.print("Resultado: ");
-//                    System.out.println(Lz77Compresor.descomprimir(input));
-//                    break;
-//                default:
-//                    System.out.println(String.format("La opcion '%s' no es valida", op));
-//            }
-//
-//            System.out.println();
-//        }
-
         CompresorTextoGUI ventana = new CompresorTextoGUI();
         ventana.setVisible(true);
     }
--- src/gui/CompresorTextoGUI.java
+++ src/gui/CompresorTextoGUI.java
@@ -3,6 +3,7 @@ package gui;
 import lz77.Lz77Compresor;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -50,12 +51,16 @@ public class CompresorTextoGUI extends JFrame {
         JButton cmdComprimirTexto = new JButton("Comprimir");
         cmdComprimirTexto.setBounds(20, 210, 200, 30);
         cmdComprimirTexto.addActionListener((e) -> comprimirTexto());
+        cmdComprimirTexto.setBackground(new Color(0, 150, 0));
+        cmdComprimirTexto.setForeground(Color.white);
         add(cmdComprimirTexto);
 
         // Button Descomprimir
         JButton cmdDescomprimirTexto = new JButton("Descomprimir");
         cmdDescomprimirTexto.setBounds(260, 210, 200, 30);
         cmdDescomprimirTexto.addActionListener((e) -> descomprimirTexto());
+        cmdDescomprimirTexto.setBackground(new Color(150, 0, 0));
+        cmdDescomprimirTexto.setForeground(Color.white);
         add(cmdDescomprimirTexto);
 
         // CompressedText
